8db92e5dbb46b5e9b9bee767f337d6fcaffe8c84
kuqmua <serezhashmakov@outlook.com>
2022-02-23 13:26:20 +0300

    some preparing

--- src/i_can_throw_one.rs
+++ src/i_can_throw_one.rs
@@ -1,3 +1,7 @@
+// pub struct ICanThrowOneError {
+//     source: bool,
+// }
+
 pub fn i_can_throw_one() -> Result<(), bool> {
     Err(false)
 }
--- src/i_can_throw_two.rs
+++ src/i_can_throw_two.rs
@@ -1,3 +1,7 @@
+// pub struct ICanThrowTwoError {
+//     source: u32,
+// }
+
 pub fn i_can_throw_two() -> Result<(), u32> {
     Err(32)
 }
