a7a9c8ca19d6ca9fd9e2c7ac79cd53b4671f078b
kuqmua <serezhashmakov@outlook.com>
2022-02-23 11:07:57 +0300

    another step!

--- macros/something/src/lib.rs
+++ macros/something/src/lib.rs
@@ -40,11 +40,6 @@ pub fn derive_exact_how_concept(input: TokenStream) -> TokenStream {
             }
             _ => panic!("v.fields is not syn::Fields::Unnamed"),
         }
-        //         where_was: WhereWas {
-        //     file: file!(),
-        //     line: line!(),
-        //     column: column!(),
-        // },
         quote! {
             impl From<#inner_enum_type> for #ident {
                 fn from(error: #inner_enum_type) -> Self {
@@ -57,52 +52,17 @@ pub fn derive_exact_how_concept(input: TokenStream) -> TokenStream {
         #(#generated)*
     };
     gen.into()
-    ////////////////////
-    // let trait_name: Ident;
-    // let function_vec_idents: Vec<(Ident, ReturnType)>;
-    // match fs::read_to_string("./src/traits/provider_kind_from_config_trait.rs") {
-    //     Err(e) => panic!("file:  error: {e}"),
-    //     Ok(file) => {
-    //         let token_stream: proc_macro::TokenStream = file
-    //             .parse()
-    //             .expect("cannot parse file into proc_macro::TokenStream");
-    //         let trait_ast: syn::ItemTrait = syn::parse(token_stream)
-    //             .expect("cannot parse token_stream from file into syn::ItemTrait");
-    //         trait_name = trait_ast.ident;
-    //         function_vec_idents = trait_ast
-    //             .items
-    //             .iter()
-    //             .filter_map(|trait_item| match trait_item {
-    //                 TraitItem::Method(trait_item_method) => Some((
-    //                     trait_item_method.sig.ident.clone(),
-    //                     trait_item_method.sig.output.clone(),
-    //                 )),
-    //                 _ => None,
-    //             })
-    //             .collect();
-    //     }
+}
+
+/////
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{:#?}\"", item);
+    //I CAN EXTEND THE ITEM!
+    // quote! {
+    //     //kekw
     // }
-    //     //                 quote! {
-    //     //                     impl From<#inner_enum_type> for #struct_ident {
-    //     //                         fn from(error: #inner_enum_type) -> Self {
-    //     //                             #struct_ident {
-    //     //                                 source: Box::new(#ident::#variant(
-    //     //                                     error,
-    //     //                                 )),
-    //     //                                 where_was: WhereWas {
-    //     //                                     file: file!(),
-    //     //                                     line: line!(),
-    //     //                                     column: column!(),
-    //     //                                 },
-    //     //                             }
-    //     //                         }
-    //     //                     }
-    //     //                 }
-    // }
-    // let generated = quote! {
-    //     impl #trait_name for #ident {
-    //         #(#function_quote_vec_ident)*
-    //     }
-    // };
-    // generated.into()
+    // .into()
+    item
 }
--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,26 +1,41 @@
 use crate::i_can_throw_one::i_can_throw_one; //open file inside proc macro
 use crate::i_can_throw_two::i_can_throw_two; //open file inside proc macro
 
+use something::show_streams;
+
 #[derive(ExactHowConceptDerive)]
 pub enum WrapperReturn {
     One(bool),
     Two(u32),
 }
 
-// impl From<bool> for WrapperReturn {
-//     fn from(e: bool) -> Self {
-//         WrapperReturn::One(e)
-//     }
-// }
-
-// impl From<u32> for WrapperReturn {
-//     fn from(e: u32) -> Self {
-//         WrapperReturn::Two(e)
-//     }
-// }
-
+#[show_streams]
 pub fn wrapper() -> Result<(), WrapperReturn> {
     i_can_throw_one()?;
     i_can_throw_two()?;
     Ok(())
 }
+
+// // Example: Basic function
+// #[show_streams]
+// fn invoke1() {}
+// // out: attr: ""
+// // out: item: "fn invoke1() { }"
+
+// // Example: Attribute with input
+// #[show_streams(bar)]
+// fn invoke2() {}
+// // out: attr: "bar"
+// // out: item: "fn invoke2() {}"
+
+// // Example: Multiple tokens in the input
+// #[show_streams(multiple => tokens)]
+// fn invoke3() {}
+// // out: attr: "multiple => tokens"
+// // out: item: "fn invoke3() {}"
+
+// // Example:
+// #[show_streams { delimiters }]
+// fn invoke4() {}
+// // out: attr: "delimiters"
+// // out: item: "fn invoke4() {}"
